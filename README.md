ee72965427415439cc8840d0b0236d3bc6609b4f
Andres-Bermudez <andres201900@outlook.com>
2024-12-02 23:02:06 -0500

    Adding proyect to GitHub

---
+++ README.md
@@ -0,0 +1,19 @@
+# Market
+
+Este proyecto esta creado con una arquitectura por capas
+orientada al dominio.
+
+Se creo un archivo .properties para un ambiente de desarrollo
+y otro diferente para un ambiente de produccion.
+
+En este proyecto se usa Gradle como gestor de dependencias.
+
+### Seguimiento del proyecto:
+1. Creacion del proyecto con Spring Boot.
+2. Configuracion del archivo .properties
+3. Creacion de la estructura del proyecto.
+4. Agregar dependencia Spring Data JPA.
+5. Crear una base de datos en PostgreSQL.
+6. Crear el esquema de la base de datos e insertar datos.
+7. Agregar el driver de PostgreSQL.
+8. Conectar la aplicacion con la base de datos.
---
+++ sql/schema.sql
@@ -0,0 +1,80 @@
+-- -----------------------------------------------------
+-- Tabla "CATEGORIAS"
+-- -----------------------------------------------------
+CREATE TABLE  categories (
+  "id_category" SERIAL NOT NULL,
+  "description" VARCHAR(45) NOT NULL,
+  "status" BOOLEAN NOT NULL,
+  PRIMARY KEY ("id_category"));
+
+
+-- -----------------------------------------------------
+-- Tabla "PRODUCTOS"
+-- -----------------------------------------------------
+CREATE TABLE  products (
+  "id_product" SERIAL NOT NULL,
+  "name" VARCHAR(45) NULL,
+  "id_category" INT NOT NULL,
+  "bar_code" VARCHAR(150) NULL,
+  "sales_price" DECIMAL(16,2) NULL,
+  "stock_quantity" INT NOT NULL,
+  "status" BOOLEAN NULL,
+  PRIMARY KEY ("id_product"),
+  CONSTRAINT "fk_products_categories"
+    FOREIGN KEY ("id_category")
+    REFERENCES categories ("id_category")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Tabla "CLIENTES"
+-- -----------------------------------------------------
+CREATE TABLE  customers (
+  "id" VARCHAR(20) NOT NULL,
+  "name" VARCHAR(40) NULL,
+  "lastname" VARCHAR(100) NULL,
+  "phone" NUMERIC NULL,
+  "address" VARCHAR(80) NULL,
+  "email" VARCHAR(70) NULL,
+  PRIMARY KEY ("id"));
+
+
+-- -----------------------------------------------------
+-- Table "COMPRAS"
+-- -----------------------------------------------------
+CREATE TABLE  purchases (
+  "id_purchase" SERIAL NOT NULL,
+  "id_customer" VARCHAR(20) NOT NULL,
+  "date" TIMESTAMP NULL,
+  "payment_method" CHAR(1) NULL,
+  "comment" VARCHAR(300) NULL,
+  "status" CHAR(1) NULL,
+  PRIMARY KEY ("id_purchase"),
+  CONSTRAINT "fk_purchases_customers1"
+    FOREIGN KEY ("id_customer")
+    REFERENCES customers ("id")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table "COMPRAS_PRODUCTOS"
+-- -----------------------------------------------------
+CREATE TABLE  purchases_products (
+  "id_purchase" INT NOT NULL,
+  "id_product" INT NOT NULL,
+  "quantity" INT NULL,
+  "total" DECIMAL(16,2) NULL,
+  "status" BOOLEAN NULL,
+  PRIMARY KEY ("id_purchase", "id_product"),
+  CONSTRAINT "fk_purchases_products_products1"
+    FOREIGN KEY ("id_product")
+    REFERENCES products ("id_product")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT "fk_purchases_products_purchases1"
+    FOREIGN KEY ("id_purchase")
+    REFERENCES purchases ("id_purchase")
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
